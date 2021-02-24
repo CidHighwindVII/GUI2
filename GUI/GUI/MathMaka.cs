c96f4029cf1346ec47c22a59b7f78de2e205c798
Carlos Leite <CarlosLeite@criticalmanufacturing.com>
2021-02-24 13:53:29 +0000

    First commit

---
+++ GUI/GUI/MathMaka.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    public class MathMaka
+    {
+        public void MakeOperations()
+        {
+            Lib1.Lib1Math.Minus(1, 1);
+            Lib2.Lib2Math.Multiply(1, 1);
+            Lib3.Lib3Math.Plus(1, 1);
+        }
+    }
+}
